f0a471bc767d4b331ab32a60ec4985e680944efc
Roy Luc <lr.infographie@gmail.com>
2018-08-31 08:03:43 -0400

    Rajout classes et constantes

---
+++ app/src/main/java/ca/cours5b5/lucroy/global/GConstantes.java
@@ -0,0 +1,14 @@
+package ca.cours5b5.lucroy.global;
+
+public class GConstantes {
+
+    final static int HMIN = 4;
+    final static int HMAX = 10;
+    final static int HDEF  = 6;
+    final static int LMIN = 7;
+    final static int LMAX = 4;
+    final static int LDEF  = 10;
+    final static int GMIN = 3;
+    final static int GMAX = 4;
+    final static int GDEF  = 4;
+}
